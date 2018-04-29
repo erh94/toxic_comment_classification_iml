c10f67ba5025e984222cfbbc81241667f0d39ad3
digiblackdranzer <ourgour@gmail.com>
2018-04-29 23:20:31 +0530

    submission update

--- submission.py
+++ submission.py
@@ -12,6 +12,8 @@ from keras.callbacks import Callback
 
 if __name__ == "__main__":
 
+	submission = pd.read_csv('./input/sample_submission.csv')
+	list_classes = ["toxic", "severe_toxic", "obscene", "threat", "insult", "identity_hate"]
 	json_file = open('./models/GRU_feat_none.json', 'r')
 	loaded_model_json = json_file.read()
 	json_file.close()
@@ -19,7 +21,11 @@ if __name__ == "__main__":
 	loaded_model.load_weights("./models/GRU_feat_none.h5")
 	print("Loaded model from disk")
 
+	with open('./models/x_test.pkl','rb') as handle: 
+		x_test = pickle.load(handle)
+
 	y_pred = loaded_model.predict(x_test, batch_size=1024)
+	y_pred = np.delete(y_pred,6,1)
 	submission[list_classes] = y_pred
-	submission.drop(['none'],axis=1)
+	#submission.drop(['none'],axis=1)
 	submission.to_csv('./output/submission_GRU_glove.csv', index=False)
